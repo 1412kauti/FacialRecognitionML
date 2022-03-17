c01b43baeeb1ce54d19d0b10b447658ccd3bc78b
Kautilya Reddy <kautilya97@gmail.com>
2022-03-17 11:49:53 +0000

    Oszkar -- rename variables and pep8 remove

--- Facial.py
+++ Facial.py
@@ -5,85 +5,89 @@
 # *-
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.uic import loadUi
-from PyQt5.QtCore import pyqtSlot, QTimer, QDate, Qt
-from PyQt5.QtWidgets import QDialog,QMessageBox
+from PyQt5.QtCore import pyqtSlot, QTimer, QDate    # Qt
+from PyQt5.QtWidgets import QDialog, QMessageBox
 import cv2
 import face_recognition
 import numpy as np
 import datetime
+# from datetime import timedelta
 import os
 import csv
 
+
+# noinspection PyPep8Naming
 class Ui_OutputDialog(QDialog):
     def __init__(self):
         super(Ui_OutputDialog, self).__init__()
         loadUi("./outputwindow.ui", self)
 
-        #Update time
+        # Update time
         now = QDate.currentDate()
         current_date = now.toString('ddd dd MMMM yyyy')
         current_time = datetime.datetime.now().strftime("%I:%M %p")
         self.Date_Label.setText(current_date)
         self.Time_Label.setText(current_time)
 
+        self.class_names = []
+        self.encode_list = []
+        self.time_list1 = []
+        self.time_list2 = []
         self.image = None
 
     @pyqtSlot()
-    def startVideo(self, camera_name):
+    def start_video(self, camera_name):
         """
         :param camera_name: link of camera or usb camera
         :return:
         """
         if len(camera_name) == 1:
-        	self.capture = cv2.VideoCapture(int(camera_name))
+            self.capture = cv2.VideoCapture(int(camera_name))
         else:
-        	self.capture = cv2.VideoCapture(camera_name)
+            # noinspection PyAttributeOutsideInit
+            self.capture = cv2.VideoCapture(camera_name)
+        # noinspection PyAttributeOutsideInit
         self.timer = QTimer(self)  # Create Timer
         path = 'ImagesAttendance'
         if not os.path.exists(path):
             os.mkdir(path)
         # known face encoding and known face name list
         images = []
-        self.class_names = []
-        self.encode_list = []
-        self.TimeList1 = []
-        self.TimeList2 = []
+
         attendance_list = os.listdir(path)
 
         # print(attendance_list)
         for cl in attendance_list:
-            cur_img = cv2.imread(f'{path}/{cl}')
-            images.append(cur_img)
+            # current_image = cv2.imread(f'{path}/{cl}')
+            images.append(cv2.imread(f'{path}/{cl}'))
             self.class_names.append(os.path.splitext(cl)[0])
         for img in images:
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             boxes = face_recognition.face_locations(img)
-            encodes_cur_frame = face_recognition.face_encodings(img, boxes)[0]
+            encodes_current_frame = face_recognition.face_encodings(img, boxes)[0]
             # encode = face_recognition.face_encodings(img)[0]
-            self.encode_list.append(encodes_cur_frame)
+            self.encode_list.append(encodes_current_frame)
         self.timer.timeout.connect(self.update_frame)  # Connect timeout to the output function
         self.timer.start(10)  # emit the timeout() signal at x=40ms
 
-    def face_rec_(self, frame, encode_list_known, class_names):
+    @staticmethod
+    def face_rec_(frame, encode_list_known, class_names):
         """
         :param frame: frame from camera
         :param encode_list_known: known face encoding
         :param class_names: known face names
         :return:
         """
-
-
-
-
+        # one_day = datetime.datetime.now() + timedelta(days=1)
         # face recognition
-        faces_cur_frame = face_recognition.face_locations(frame)
-        encodes_cur_frame = face_recognition.face_encodings(frame, faces_cur_frame)
+        faces_current_frame = face_recognition.face_locations(frame)
+        encodes_current_frame = face_recognition.face_encodings(frame, faces_current_frame)
         # count = 0
-        for encodeFace, faceLoc in zip(encodes_cur_frame, faces_cur_frame):
+        for encodeFace, faceLoc in zip(encodes_current_frame, faces_current_frame):
             match = face_recognition.compare_faces(encode_list_known, encodeFace, tolerance=0.50)
-            face_dis = face_recognition.face_distance(encode_list_known, encodeFace)
-            name = "unknown"
-            best_match_index = np.argmin(face_dis)
+            face_distance = face_recognition.face_distance(encode_list_known, encodeFace)
+            # name = "unknown"
+            best_match_index = np.argmin(face_distance)
             # print("s",best_match_index)
             if match[best_match_index]:
                 name = class_names[best_match_index].upper()
@@ -92,20 +96,23 @@ class Ui_OutputDialog(QDialog):
                 cv2.rectangle(frame, (x1, y2 - 20), (x2, y2), (0, 255, 0), cv2.FILLED)
                 cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
                 with open('Attandence.csv', 'r+') as f:
-                    myDataList = f.readlines()
-                    nameList = []
-                    for line in myDataList:
+                    my_data_list = f.readlines()
+                    # nameList = dict
+                    name_list = []
+                    for line in my_data_list:
                         entry = line.split(',')
-                        nameList.append(entry[0])
-                    if name not in nameList:
+                        name_list.append(entry[0])  # nameList.update({entry[0]:entry[1]})
+                        # nameList.append(entry[0])
+                    if name not in name_list:
+                        # if name not in nameList.keys(self) and nameList.values(self) == one_day:
                         now = datetime.datetime.now()
-                        dtString = now.strftime('%H:%M:%S')
-                        f.writelines(f'\n{name},{dtString}')
-
+                        dt_string = now.strftime('%H:%M:%S')
+                        f.writelines(f'\n{name},{dt_string}')
 
         return frame
 
-    def showdialog(self):
+    @staticmethod
+    def show_dialog():
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Information)
 
@@ -115,45 +122,33 @@ class Ui_OutputDialog(QDialog):
         msg.setDetailedText("The details are as follows:")
         msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
 
-
-    def ElapseList(self,name):
+    def elapse_list(self, name):
         with open('Attendance.csv', "r") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-            line_count = 2
+            # line_count = 2
 
-            Time1 = datetime.datetime.now()
-            Time2 = datetime.datetime.now()
+            # Time1 = datetime.datetime.now()
+            # Time2 = datetime.datetime.now()
             for row in csv_reader:
                 for field in row:
                     if field in row:
                         if field == 'Clock In':
                             if row[0] == name:
-                                #print(f'\t ROW 0 {row[0]}  ROW 1 {row[1]} ROW2 {row[2]}.')
-                                Time1 = (datetime.datetime.strptime(row[1], '%y/%m/%d %H:%M:%S'))
-                                self.TimeList1.append(Time1)
+                                # print(f'\t ROW 0 {row[0]}  ROW 1 {row[1]} ROW2 {row[2]}.')
+                                time1 = (datetime.datetime.strptime(row[1], '%y/%m/%d %H:%M:%S'))
+                                self.time_list1.append(time1)
                         if field == 'Clock Out':
                             if row[0] == name:
-                                #print(f'\t ROW 0 {row[0]}  ROW 1 {row[1]} ROW2 {row[2]}.')
-                                Time2 = (datetime.datetime.strptime(row[1], '%y/%m/%d %H:%M:%S'))
-                                self.TimeList2.append(Time2)
-                                #print(Time2)
-
-
-
-
+                                # print(f'\t ROW 0 {row[0]}  ROW 1 {row[1]} ROW2 {row[2]}.')
+                                time2 = (datetime.datetime.strptime(row[1], '%y/%m/%d %H:%M:%S'))
+                                self.TimeList2.append(time2)
+                                # print(Time2)
 
     def update_frame(self):
         ret, self.image = self.capture.read()
-        self.displayImage(self.image, self.encode_list, self.class_names, 1)
+        self.display_image(self.image, self.encode_list, self.class_names, 1)
 
-    def displayImage(self, image, encode_list, class_names, window=1):
-        """
-        :param image: frame from camera
-        :param encode_list: known face encoding list
-        :param class_names: known face names
-        :param window: number of window
-        :return:
-        """
+    def display_image(self, image, encode_list, class_names, window=1):
         image = cv2.resize(image, (640, 480))
         try:
             image = self.face_rec_(image, encode_list, class_names)
@@ -165,9 +160,9 @@ class Ui_OutputDialog(QDialog):
                 qformat = QImage.Format_RGBA8888
             else:
                 qformat = QImage.Format_RGB888
-        outImage = QImage(image, image.shape[1], image.shape[0], image.strides[0], qformat)
-        outImage = outImage.rgbSwapped()
+        out_image = QImage(image, image.shape[1], image.shape[0], image.strides[0], qformat)
+        out_image = out_image.rgbSwapped()
 
         if window == 1:
-            self.imgLabel.setPixmap(QPixmap.fromImage(outImage))
+            self.imgLabel.setPixmap(QPixmap.fromImage(out_image))
             self.imgLabel.setScaledContents(True)
--- starter.py
+++ starter.py
@@ -1,39 +1,41 @@
 import sys
 from PyQt5.uic import loadUi
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import pyqtSlot
+# from PyQt5 import QtWidgets
+# from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QDialog
-import resource
+# import resource
 from Facial import Ui_OutputDialog
+
 
 class Attendance(QDialog):
     def __init__(self):
-        super(Attendance,self).__init__()
+        super(Attendance, self).__init__()
         loadUi("outputwindow.ui")
         self._new_window = None
-        self.Videocapture_ = None
-        self.runSlot()
-    
-    def refreshAll(self):
+        self.video_capture = None
+        self.run_slot()
+
+    def refresh_all(self):
         """
         Set the text of lineEdit once it's valid
         """
-        self.Videocapture_ = "0"
-    
-    def runSlot(self):
+        self.video_capture = "0"
+
+    def run_slot(self):
         """
         Called when the user presses the Run button
         """
-        self.refreshAll()
-        self.outputWindow_()  # Create and open new output window
+        self.refresh_all()
+        self.output_window()  # Create and open new output window
 
-    def outputWindow_(self):
+    def output_window(self):
         """
         Created new window for vidual output of the video in GUI
         """
         self._new_window = Ui_OutputDialog()
         self._new_window.show()
-        self._new_window.startVideo(self.Videocapture_)
+        self._new_window.start_video(self.video_capture)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
